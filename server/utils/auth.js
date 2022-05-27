1662eac4299fa364c15edd5c776aa60d77e81537
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-27 16:41:01 -0400

    converted to work with apollo requests

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,31 +5,29 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  authMiddleware: function ({ req }) { // function for our authenticated routes
-    // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+  authMiddleware: function ({req}, res) { // function for authenticated routes
+    console.log('Auth Test 1:', req.body, req.query, req.headers);
+    // allows token to be sent via req.query or headers - expect headers
+    let token = req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    if (!token) { // return req if no Auth - will work for loginUser and addUser resolvers
+      return req
     }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-      // return req
+    if (req.headers.authorization) { // if token was sent through req.headers.authorization
+      token = token.split(' ').pop().trim(); // ["Bearer", "<tokenvalue>"]
     }
-
-    // verify token and get user data out of it
-    try {
+    
+    try { // verify token and get user data out of it
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
+      console.log('req.user:', req.user);
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // // send to next endpoint
+    // next();
+    return req.user;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
