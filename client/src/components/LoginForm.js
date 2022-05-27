2ec07cb5314e6234ada055de14a77d2c17aba612
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-27 16:39:04 -0400

    loginUser functional

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -5,10 +5,15 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations'
+
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [login, { data, error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -24,17 +29,18 @@ const LoginForm = () => {
       event.preventDefault();
       event.stopPropagation();
     }
-
     try {
-      const response = await loginUser(userFormData);
+      // const response = await loginUser(userFormData);
+      console.log('test1', userFormData);
+      const { data } = await login({ variables: { ...userFormData } });
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      console.log('test3', data);
+      // Auth.login(token);
+      Auth.login(data.loginUser.token)
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -85,6 +91,11 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">
+          {error.message}
+        </div>
+      )}
     </>
   );
 };
