b295a091fa77e4c6c3903bf7f7e0118dfda29a27
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-27 16:40:20 -0400

    GET_ME added

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+query getMe($username: String, $meId: String) {
+  me(username: $username, id: $meId) {
+    _id
+    username
+    email
+    password
+    savedBoods {
+      _id
+      authors
+      description
+      bookId
+      image
+      link
+      title
+    }
+  }
+}`;
