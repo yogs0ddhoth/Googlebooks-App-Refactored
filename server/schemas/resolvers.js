87043a0179172788621e476a5b995df61ab3a3e5
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-27 16:42:18 -0400

    'me' added, 'addUser', 'loginUser' functional

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,39 @@
 const { User } = require('../models');
-const { signToken } = require('../utils/auth')
-export const resolvers = {
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const resolvers = {
   Query: {
-    me: async (parent, ) =>{
+    me: async (parent, args, context) =>{
+      console.log('Query-Test Context:', context);
       return User.findOne(
-        {}
-      )
+        { $or: [{ _id: _id}, { username: username }] }
+      ).populate('books');
     }
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    loginUser: async (parent,  { email, password }) => {
+      
+      const user = await User.findOne( { email: email } );
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // saveBook: async (parent, ())
   }
 }
+
+module.exports = resolvers;
