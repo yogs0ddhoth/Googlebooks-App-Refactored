f23c82c95a874100d81be8cf95f9afefcaa30dfe
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-28 23:38:28 -0400

    connection updated for deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27107/googlebooks-graphql-app', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
