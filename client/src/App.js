cec05ee6dc0caaa3721bf2436b87f2334c2d9a62
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-28 23:35:06 -0400

    ApolloProvider and Client connected, context set

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({ uri: '/graphql' });
+
+const authlink= setContext((_, { headers }) => {
+  console.log(httpLink);
+  const token = localStorage.getItem('id_token');
+  console.log('Payload:', {headers: { 
+      ...headers, 
+      authorization: token ? `Bearer ${token}` : '' 
+    }})
+  return { 
+    headers: { 
+      ...headers, 
+      authorization: token ? `Bearer ${token}` : '' 
+    } 
+  };
+})
+const client = new ApolloClient(
+  { link: authlink.concat(httpLink), cache: new InMemoryCache() }
+);
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+      <Router>
       <>
         <Navbar />
         <Routes>
@@ -24,8 +54,9 @@ function App() {
           />
         </Routes>
       </>
-    </Router>
-  );
+      </Router>
+    </ApolloProvider>
+  )
 }
 
 export default App;
