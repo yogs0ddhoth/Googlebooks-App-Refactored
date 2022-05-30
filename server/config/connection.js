7bce4f7580e8b10d163178b3d41ea4b8cc025b19
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-30 16:35:13 -0400

    local mongoose config option added for dev

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27107/googlebooks-graphql-app', {
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  // // For deployed version:
+  // 'mongodb://localhost:27107/googlebooks-graphql-app', {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // } 
+  // Use for local development:
+  'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-});
+  }
+);
 
 module.exports = mongoose.connection;
