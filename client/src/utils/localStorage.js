849449d97fdd045f0fc9a5ec8ec6d72337ee01d1
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-30 21:32:42 -0400

    comments added

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,30 +1,27 @@
-export const getSavedBookIds = () => {
+export const getSavedBookIds = () => { 
+  // if 'saved_books' item exists in local storage, assign it to const savedBookIds and return
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
-    : [];
-
+    : []; // if 'saved_objects' does not exists, assign an empty array
   return savedBookIds;
 };
 
-export const saveBookIds = (bookIdArr) => {
-  if (bookIdArr.length) {
-    localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
-  } else {
-    localStorage.removeItem('saved_books');
-  }
+export const saveBookIds = (bookIdArr) => { // expect an array of strings
+  bookIdArr.length // if the argument is an array with items, set it to 'saved_books' in local storage
+    ? localStorage.setItem('saved_books', JSON.stringify(bookIdArr)) 
+    : localStorage.removeItem('saved_books') // else, remove 'saved_books' from local storage
 };
 
-export const removeBookId = (bookId) => {
+export const removeBookId = (bookId) => { // expect a string
+  // if 'saved_books' item exists in local storage, assign it to const savedBookIds
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
-    : null;
-
+    : null; // if 'saved_objects' does not exists, assign null
   if (!savedBookIds) {
-    return false;
-  }
-
+    return false; // if null, return false
+  } // asign savedBookIds filtered to remove items that match the argument 'bookId'
   const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
-  localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
-
+  localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds)); //stringify the array and save to local storage
+  // return true if the operation is successful
   return true;
 };
