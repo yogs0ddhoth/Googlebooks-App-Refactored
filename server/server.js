e8a21f6298aa117c4284a9009f15739485715b4b
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-30 21:09:13 -0400

    unused routes removed

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth')
-const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,12 +21,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')))
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
