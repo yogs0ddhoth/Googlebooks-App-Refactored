f2d7e873c7302a2d7113ef702eb38418693b04dc
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-30 21:08:17 -0400

    comments updated

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,31 +7,24 @@ const expiration = '2h';
 module.exports = {
   authMiddleware: function ({req}, res) { // function for our authenticated routes
     console.log('Auth Test 1:', req.body, req.query, req.headers);
-    // allows token to be sent via  req.query or headers
+    // allows token to be sent via req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-
-    if (!token) { // return req if no Auth - will work for loginUser and addUser
+    if (!token) { // return req if no token -> allows loginUser and addUser to work without token
       return req
     }
-
     if (req.headers.authorization) { // if token was sent through req.headers.authorization
       token = token.split(' ').pop().trim(); // ["Bearer", "<tokenvalue>"]
     }
-
-    // verify token and get user data out of it
-    try {
+    try { // verify token and get user data out of it
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-      console.log('req.user:', req.user);
     } catch {
       console.log('Invalid token');
     }
-
-    // // send to next endpoint
-    // next();
+    // send to next endpoint
     return req.user;
   },
-  signToken: function ({ username, email, _id }) {
+  signToken: function ({ username, email, _id }) { // create and return token
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
