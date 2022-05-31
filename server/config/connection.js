9a895d0e3926733e971efa932f804528a4c42c2d
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-30 21:06:09 -0400

    updated for production

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-  // // For deployed version:
-  // 'mongodb://localhost:27107/googlebooks-graphql-app', {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-  // } 
-  // Use for local development:
-  'mongodb://localhost/googlebooks', {
+  // For deployed version:
+  'mongodb://localhost:27107/googlebooks-graphql-app', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  }
+  } 
+  // // Use for local development:
+  // 'mongodb://localhost/googlebooks', {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // }
 );
 
 module.exports = mongoose.connection;
